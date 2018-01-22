0ea0f25a21c71cd2ca86cabc65785a5602f44f1b
Vinicius Cechin <vinicechin@gmail.com>
2018-01-22 18:51:33 -0200

    Added Section 2 Lecture 23 changes - computed property

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 new Vue({
   el: '#app',
   data: {
-    name: 'Vinicius'
+    counter: 0,
+    secondCounter: 0
+  },
+  computed: {
+    output: function() {
+      console.log('Computed');
+      return this.counter > 5 ? 'Greater 5' : 'Smaller 5';      
+    }
+  },
+  methods: {
+    result: function() {
+      console.log('Method');
+      return this.counter > 5 ? 'Greater 5' : 'Smaller 5';
+    }
   }
 })
