7d1c33bc8348bbfcdf4cb4f6b9e7a399aa5b8f05
Vinicius Cechin <vinicechin@gmail.com>
2018-01-22 16:30:06 -0200

    Added Section 2 Lecture 17 changes - listeners

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 new Vue({
   el: '#app',
   data: {
-    counter: 0
+    counter: 0,
+    x: 0,
+    y: 0
   },
   methods: {
     increase: function() {
       this.counter++;
+    },
+
+    updateCoordinates: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
     }
   }
 })
