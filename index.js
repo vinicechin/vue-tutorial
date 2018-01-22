6016391abb828bc83659eda40a8c9741b44362e8
Vinicius Cechin <vinicechin@gmail.com>
2018-01-22 15:18:37 -0200

    Added Section 2 Lecture 11 changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 new Vue({
   el: '#app',
   data: {
-    title: ""
+    title: "Hello World!"
   },
   methods: {
-    changeTitle: function(event) {
-      this.title = event.target.value;
+    sayHello: function() {
+      return 'Hello!';
     }
-  } 
+  }
 })
