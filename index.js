963505e4f2db0ed9af2341e90423bc8e9633bd0c
Vinicius Cechin <vinicechin@gmail.com>
2018-01-23 10:41:01 -0200

    Added Section 2 Lecture 25 - shorthands

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 new Vue({
   el: '#app',
   data: {
-    counter: 0,
-    secondCounter: 0
-  },
-  computed: {
-    output: function() {
-      console.log('Computed');
-      return this.counter > 5 ? 'Greater 5' : 'Smaller 5';      
-    }
-  },
-  watch: {
-    counter: function(value) {
-      var vm = this;
-      setTimeout(function() {
-        vm.counter = 0;
-      }, 2000);
-    }
+    link: 'http://google.com'
   },
   methods: {
-    result: function() {
-      console.log('Method');
-      return this.counter > 5 ? 'Greater 5' : 'Smaller 5';
+    changeLink: function() {
+      this.link = 'http://apple.com';
     }
   }
 })
