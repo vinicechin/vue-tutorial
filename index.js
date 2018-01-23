addb89e1e89818260d27317ebfb9d981fb2188e6
Vinicius Cechin <vinicechin@gmail.com>
2018-01-23 15:48:07 -0200

    Added Section 3 Lecture 41
    v-for loop over objects. (<value>, <value-key>, <internal-obj-index>)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ new Vue({
   el: '#app',
   data: {
     ingredients: ['meat', 'fruit', 'cookies'],
-    person: [
+    persons: [
       {name: 'Max', age: 27, color: 'red'},
       {name: 'Anna', age: 'unknown', color: 'blue'}
     ]
