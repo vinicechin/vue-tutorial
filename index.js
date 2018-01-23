d6ddc124a958295e75f33caf925d7dfd49b7da92
Vinicius Cechin <vinicechin@gmail.com>
2018-01-23 11:15:00 -0200

    Added Section 2 Lecture 26 - dynamic css styling

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 new Vue({
   el: '#app',
   data: {
-    link: 'http://google.com'
-  },
-  methods: {
-    changeLink: function() {
-      this.link = 'http://apple.com';
-    }
+    attach: false
   }
-})
+});
