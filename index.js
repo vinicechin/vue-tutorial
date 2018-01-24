29ced6477c2e259f07b923eda39e8a3dcb3cca03
Vinicius Cechin <vinicechin@gmail.com>
2018-01-24 13:55:31 -0200

    Added Section 5 Lecture 66
    Elements management by VueJS object

--- index.js
+++ index.js
@@ -30,8 +30,13 @@ var vm1 = new Vue({
 
 });
 
+// new property is not watched by VueJS
+vm1.newProp = 'New!';
+console.log(vm1);
+
 setTimeout(function() {
   vm1.title = 'Changed by Timer!';
+  vm1.show();
 } ,3000);
 
 var vm2 = new Vue({
