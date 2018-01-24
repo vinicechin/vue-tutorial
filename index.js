05f4399aa05d7fefd304e8475855567b3f0fc531
Vinicius Cechin <vinicechin@gmail.com>
2018-01-24 15:59:30 -0200

    Added Section 5 Lecture 75
    VueJS lifecycle

--- index.js
+++ index.js
@@ -1,67 +1,45 @@
-var data = {
-  title: 'The VueJS Instance',
-  showParagraph: false
-}
+new Vue({
+  el: '#app',
+  data: {
+    title: 'The VueJS Instance'
+  },
 
-Vue.component('hello', {
-  template: '<h1>Hello!</h1>'
-});
+  beforeCreate: function() {
+    console.log('beforeCreate()');
+  },
 
-var vm1 = new Vue({
-  data: data,
+  created: function() {
+    console.log('created()');
+  },
+
+  beforeMount: function() {
+    console.log('beforeMount()');
+  },
+
+  mounted: function() {
+    console.log('mounted()');
+  },
+
+  beforeUpdate: function() {
+    console.log('beforeUpdate()');
+  },
+
+  updated: function() {
+    console.log('updated()');
+  },
+
+  beforeDestroy: function() {
+    console.log('beforeDestroy()');
+  },
+
+  destroyed: function() {
+    console.log('destroyed()');
+  },
 
   methods: {
-    show: function() {
-      this.showParagraph = true;
-      this.updateTitle('The VueJS Instance (Updated)');
-      this.$refs.myButton.innerText = 'Test';
-    },
-
-    updateTitle: function(title) {
-      this.title = title;
-    }
-  },
-
-  conputed: {
-    lowercaseTitle: function() {
-      return this.title.toLowerCase();
-    }
-  },
-
-  watch: {
-    title: function(value) {
-      alert('Title changed, new value: ' + value);
-    }
-  }
-
-});
-
-vm1.$mount('#app1');
-
-console.log(vm1.$data === data);
-vm1.$refs.heading.innerText = 'Something else';
-
-setTimeout(function() {
-  vm1.title = 'Changed by Timer!';
-  vm1.show();
-} ,3000);
-
-var vm2 = new Vue({
-  el: '#app2',
-  data: {
-    title: 'The Second Instance'
-  },
-  methods : {
-    onChange: function() {
-      vm1.title = 'Changed!';
+    destroy: function() {
+      this.$destroy();
     }
   }
 });
-
-var vm3 = new Vue({
-  template: '<h1>Hello!</h1>'
-});
-
-vm3.$mount();
-document.getElementById('app3').appendChild(vm3.$el);
 
