be964b93775f62019f677257455edaeacdd93b21
Vinicius Cechin <vinicechin@gmail.com>
2018-01-24 11:02:45 -0200

    Added changes on project
    Added action logs, victory conditions and styling.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ new Vue({
   el: '#app',
   data: {
     isStarted: false,
+    isFirstMove: true,
     log: [],
     playerHealth: 100,
     monsterHealth: 100
@@ -12,25 +13,32 @@ new Vue({
       return this.log.length > 0;
     },
     playerHealthStyle: function() {
+      var backColor = 'green';
+      if (this.playerHealth <= 50 & this.playerHealth > 20) {
+        backColor = 'orange';
+      } else if (this.playerHealth <= 20) {
+        backColor = 'red';
+      }
       return {
-        width: this.playerHealth + '%'
+        width: this.playerHealth + '%',
+        backgroundColor: backColor
       }
     },
     monsterHealthStyle: function() {
+      var backColor = 'green';
+      if (this.monsterHealth <= 50 & this.monsterHealth > 20) {
+        backColor = 'orange';
+      } else if (this.monsterHealth <= 20) {
+        backColor = 'red';
+      }
       return {
-        width: this.monsterHealth + '%'
+        width: this.monsterHealth + '%',
+        backgroundColor: backColor
       }
     }
   },
 
   methods: {
-    resetFight: function() {
-      this.isStarted = !this.isStarted;
-      this.log = [];
-      this.playerHealth = 100;
-      this.monsterHealth = 100;
-    },
-
     playTurn: function() {
       switch (event.target.id) {
         case 'attack': 
@@ -51,26 +59,37 @@ new Vue({
 
     attack: function() {
       var value = this.playDice();
+      this.logAction(true, true, value);
       value = this.monsterHealth - value;
       if (value <= 0) {
         this.monsterHealth = 0;
       } else {
-        this.monsterHealth = value;        
+        this.monsterHealth = value;
       }
+      if (this.isFirstMove) {
+        this.isFirstMove = false;
+      }
+      this.verifyVictory(true);
     },
 
     specialAttack: function() {
       var value = this.playDice() + 2;
+      this.logAction(true, true, value);
       value = this.monsterHealth - value;
       if (value <= 0) {
         this.monsterHealth = 0;
       } else {
-        this.monsterHealth = value;        
+        this.monsterHealth = value;
       }
+      if (this.isFirstMove) {
+        this.isFirstMove = false;
+      }
+      this.verifyVictory(true);
     },
 
     heal: function() {
       var value = this.playDice();
+      this.logAction(true, false, value);
       if (value < 8) {
         value += 2;
       }
@@ -80,23 +99,85 @@ new Vue({
       } else {
         this.playerHealth = value;
       }
+      if (this.isFirstMove) {
+        this.isFirstMove = false;
+      }
     },
 
     monsterAttack: function() {
+      if (this.isFirstMove) {
+        return;
+      }
       var vm = this;
       var value = this.playDice();
+      this.logAction(false, true, value);
       value = this.playerHealth - value;
       setTimeout(function() {
-        if (value <= 0) {
+        if (value <= 0 & vm.isStarted) {
           vm.playerHealth = 0;
         } else {
-          vm.playerHealth = value;        
+          vm.playerHealth = value;
         }
-      }, 200);   
+        vm.verifyVictory(false);
+      }, 200);
     },
 
     playDice: function() {
       return Math.floor(Math.random() * 10) + 1;
+    },
+
+    resetFight: function() {
+      this.isStarted = !this.isStarted;
+      this.isFirstMove = true;
+      this.log = [];
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+
+    verifyVictory: function(isPlayer) {
+      var vm = this;
+      var hasWon = false;
+      var message;
+      if (isPlayer) {
+        hasWon = this.monsterHealth <= 0 ? true : false;
+        message = 'You won!';
+      } else {
+        hasWon = this.playerHealth <= 0 ? true : false;
+        message = 'You lost!';
+      }
+
+      if (hasWon) {
+        this.isFirstMove = true;
+        setTimeout(function() {
+          vm.resetFight();
+          if (confirm(message + " New Game?")) {
+            vm.isStarted = !vm.isStarted;
+          }
+        }, 200);
+      }
+    },
+
+    logAction: function(isPlayer, isAttack, hitPoints) {
+      var action;
+      if (isPlayer) {
+        if (isAttack) {
+          action = {
+            isPlayer: isPlayer,
+            message: 'PLAYER hits MONSTER for ' + hitPoints
+          };
+        } else {
+          action = {
+            isPlayer: isPlayer,
+            message: 'PLAYER heals himself for ' + hitPoints
+          }
+        }
+      } else {
+          action = {
+            isPlayer: isPlayer,
+            message: 'MONSTER hits PLAYER for ' + hitPoints
+          }
+      }
+      this.log.unshift(action);
     }
   }
 });
