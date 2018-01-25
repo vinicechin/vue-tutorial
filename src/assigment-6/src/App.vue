0a52035c11a835714016756a2073fcd17c9c9fca
Vinicius Cechin <vinicechin@gmail.com>
2018-01-25 13:33:15 -0200

    Created assigment-6 folder
    Added folder to keep assigment files separated

---
+++ src/assigment-6/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <container></container>
+</template>
+
+<script>
+  import Home from './Home.vue'
+  export default {
+    components: {
+      'container': Home
+    }
+  }
+</script>
+
+<style>
+
+</style>
