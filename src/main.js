8beefae04c79b3b5d0aeae514e65f8dc8bfec3e0
Vinicius Cechin <vinicechin@gmail.com>
2018-01-25 11:14:42 -0200

    Added Section 7 Lecture 92
    Data storing in components

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//var data = { status: 'Critical' }
+
 Vue.component('my-cmp', {
   data: function() {
     return {
       status: 'Critical'
     }
   },
-  template: '<p>Server Status: {{ status }}</p>'
+  template: '<p>Server Status: {{ status }} <button @click="changeStatus">Change</button></p>',
+  methods: {
+    changeStatus: function() {
+      this.status = 'Normal';
+    }
+  }
 });
 
 new Vue({
