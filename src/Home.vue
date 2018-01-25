2cf2e37269456244112dec811997b03dcf8afd80
Vinicius Cechin <vinicechin@gmail.com>
2018-01-25 12:04:11 -0200

    Added Section 7 Lecture 96
    Using components locally

--- src/Home.vue
+++ src/Home.vue
@@ -1,22 +1,14 @@
 <template>
   <div>
-    <p>Server Status: {{ status }}</p>
-    <hr>
-    <button @click="changeStatus">Change Status</button>
+    <app-server-status v-for="server in 5"></app-server-status>
   </div>
 </template>
 
 <script>
+  import ServerStatus from './ServerStatus.vue'
   export default {
-    data: function() {
-      return {
-        status: 'Critical'
-      }
-    },
-    methods: {
-      changeStatus() {
-        this.status = 'Normal';
-      }
+    components: {
+      'app-server-status': ServerStatus
     }
   }
 </script>
