3fa7905998d0a229ea5cd5e6f77e5a3a7491e282
Vinicius Cechin <vinicechin@gmail.com>
2018-01-25 16:29:37 -0200

    Added Assigment 7 files

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
-    <container></container>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <servers></servers>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
-  import Home from './components/Home.vue'
-  export default {
-    components: {
-      'container': Home
+    import Header from './components/Shared/Header.vue';
+    import Footer from './components/Shared/Footer.vue';
+    import Servers from './components/Server/Servers.vue';
+    import ServerDetails from './components/Server/ServerDetails.vue';
+
+    export default {
+        components: {
+            appHeader: Header,
+            Servers,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        }
     }
-  }
 </script>
+
+<style>
+
+</style>
