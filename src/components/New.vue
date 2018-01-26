8ff10850e43fdd2a442d58d113c7500072a08900
Vinicius Cechin <vinicechin@gmail.com>
2018-01-26 12:02:04 -0200

    Added Lecture 126
    Dynamic component default behavior

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <h3>New Quote</h3>
+    <button @click="counter++">Increase</button>
+    <p>{{ counter }}</p>
   </div>  
 </template>
 
 <script>
   export default {
+    data: function() {
+      return {
+        counter: 0
+      }
+    },
+    destroyed() {
+      console.log("component destroyed");
+    }
   }
 </script>
 
