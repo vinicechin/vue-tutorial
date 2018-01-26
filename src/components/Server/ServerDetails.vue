28182971f23b196bbfc9ba727f65565af3316ed0
Vinicius Cechin <vinicechin@gmail.com>
2018-01-25 22:36:01 -0200

    Finished assigment 7
    Added changes to complete assigment

--- src/components/Server/ServerDetails.vue
+++ src/components/Server/ServerDetails.vue
@@ -1,11 +1,31 @@
 <template>
     <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+        <p v-if="!server">Please select a server</p>
+        <p v-else>
+        	<ul>
+        		<li>{{ server.id }}</li>
+        		<li>{{ server.status }}</li>        		
+        	</ul>
+        </p>
     </div>
 
 </template>
 
 <script>
+	import { eventBus } from '../../main';
+
+	export default {
+		data: function() {
+			return {
+				server: null
+			}
+		},
+		created() {
+			eventBus.$on('serverSelected', (server) => {
+				this.server = server;
+			})
+		}
+	}
 </script>
 
 <style>
--- src/components/Server/Servers.vue
+++ src/components/Server/Servers.vue
@@ -1,16 +1,31 @@
 <template>
     <div class="col-xs-12 col-sm-6">
         <ul class="list-group">
-            <li
-                    class="list-group-item"
-                    v-for="index in 5">
-                Server #{{ index }}
-            </li>
+            <server :server="server" :key="server.id" v-for="server in servers"></server>
         </ul>
     </div>
 </template>
 
 <script>
+	import Server from './Server.vue';
+
+	export default {
+		data: function() {
+			return {
+				servers: [
+					{ id: 1, status: 'Normal'},
+					{ id: 2, status: 'Critical'},
+					{ id: 3, status: 'Unknown'},
+					{ id: 4, status: 'Normal'},
+					{ id: 5, status: 'Unknown'}
+				]
+			}
+		},
+
+		components: {
+			server: Server
+		}
+	}
 </script>
 
 <style>
