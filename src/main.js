85a2b530c4f8c65d75e3e47a71f7671ddb936d6e
Vinicius Cechin <vinicechin@gmail.com>
2018-01-26 10:35:43 -0200

    Start of new Section
    Refactore folders structure to only have files related to section 9

--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-export const eventBus = new Vue({
-	methods: {
-		serverSelect(server) {
-			this.$emit('serverSelected', server);
-		}
-	}
-});
-
 new Vue({
   el: '#app',
   render: h => h(App)
