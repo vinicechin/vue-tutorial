cfe0db4496687108f44b193c2e34309e346eff2d
Vinicius Cechin <vinicechin@gmail.com>
2018-01-29 15:23:31 -0200

    Added assigment 9 files

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,46 @@
 <template>
     <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <!-- Exercise 1 -->
+                    <!-- Create a Signup Form where you retrieve the following Information -->
+                    <!-- Full Name (First Name + Last Name) -->
+                    <!-- Mail -->
+                    <!-- Password -->
+                    <!-- Store Data? Yes/No -->
+
+                    <!-- Exercise 2 -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+                </div>
+            </div>
+        </form>
+        <hr>
         <div class="row">
-            <div class="col-xs-12">
-                <br>
-                <button class="btn btn-primary" @click="selectComponent">
-                  Load Blue Template
-                </button>
-                <button class="btn btn-success" @click="selectComponent">
-                  Load Green Template
-                </button>
-                <button class="btn btn-danger" @click="selectComponent">
-                  Load Red Template
-                </button>
-                <hr>
-                <components :is="compSelected">
-                  <p>{{ color }} component</p>
-                </components>
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: </p>
+                        <p>Mail: </p>
+                        <p>Password: </p>
+                        <p>Store in Database?: </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Blue from './components/Blue.vue';
-    import Green from './components/Green.vue';
-    import Red from './components/Red.vue';
-
     export default {
-        data: function() {
-          return {
-            compSelected: 'appBlue',
-            color: 'Blue'
-          }
-        },
-        components: {
-            appBlue: Blue,
-            appGreen: Green,
-            appRed: Red
-        },
-        methods: {
-          selectComponent(event) {
-            if (event.target.classList.contains('btn-primary')) {
-              this.compSelected = 'appBlue';
-              this.color = 'Blue';              
-            } else if (event.target.classList.contains('btn-success')) {
-              this.compSelected = 'appGreen';
-              this.color = 'Green';    
-            } else {
-              this.compSelected = 'appRed';
-              this.color = 'Red';  
-            }
-          }
-        }
     }
 </script>
 
