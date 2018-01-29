bb69797449c3b2cc4d74ffc7d842080958b89d45
Vinicius Cechin <vinicechin@gmail.com>
2018-01-29 13:39:33 -0200

    Added Lecture 147
    Binding textarea and keeping linebreakes

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,8 @@
                     <textarea
                             id="message"
                             rows="5"
-                            class="form-control"></textarea>
+                            class="form-control"
+                            v-model="message"></textarea>
                 </div>
             </div>
             <div class="row">
@@ -107,7 +108,7 @@
                         <p>Mail: {{ userData.email }}</p>
                         <p>Password: {{ userData.password }}</p>
                         <p>Age: {{ userData.age }}</p>
-                        <p>Message: </p>
+                        <p style="white-space: pre">Message: {{ message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
                             <li></li>
@@ -130,7 +131,8 @@
             email: '',
             password: '',
             age: 29
-          }
+          },
+          message: 'Type text'
         }
       }
     }
