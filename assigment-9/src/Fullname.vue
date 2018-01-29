0aa6f9033e30b11c31564cbb9a5209c7f869c124
Vinicius Cechin <vinicechin@gmail.com>
2018-01-29 16:26:26 -0200

    Added assigment-9 folder

---
+++ assigment-9/src/Fullname.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="form-group">
+        <label for="fisrtname">First Name</label>
+        <input type="text" id="firstname" class="form-control" @input="setFirstname">
+    </div>
+    <div class="form-group">
+        <label for="lastname">Last Name</label>
+        <input type="text" id="lastname" class="form-control" @input="setLastname">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['value'],
+    methods: {
+      setFirstname(event) {
+        this.value.firstname = event.target.value;
+        this.emitChange();
+      },
+
+      setLastname(event) {
+        this.value.lastname = event.target.value;
+        this.emitChange();
+      },
+
+      emitChange() {
+        this.$emit('input', this.value);
+      }
+    }
+  }
+</script>
