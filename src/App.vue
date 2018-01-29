f740fc62146f51a1787f27753814de63b2498c47
Vinicius Cechin <vinicechin@gmail.com>
2018-01-29 13:21:28 -0200

    Added Lecture 145
    Grouping data

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,23 @@
                                 type="text"
                                 id="email"
                                 class="form-control"
-                                v-model="email">
+                                v-model="userData.email">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
                         <input
                                 type="password"
                                 id="password"
-                                class="form-control">
+                                class="form-control"
+                                v-model="userData.password">
                     </div>
                     <div class="form-group">
                         <label for="age">Age</label>
                         <input
                                 type="number"
                                 id="age"
-                                class="form-control">
+                                class="form-control"
+                                v-model="userData.age">
                     </div>
 
                 </div>
@@ -102,9 +104,9 @@
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Mail: {{ email }}</p>
-                        <p>Password:</p>
-                        <p>Age:</p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Age: {{ userData.age }}</p>
                         <p>Message: </p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
@@ -124,7 +126,11 @@
     export default {
       data() {
         return {
-          email: ''
+          userData: {
+            email: '',
+            password: '',
+            age: 29
+          }
         }
       }
     }
