5c97a8e1d75a0229e72cf9dbc3d50daa68fe7bf6
Vinicius Cechin <vinicechin@gmail.com>
2018-01-30 11:01:24 -0200

    Added Lecture 160
    Passing values to custom directive

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 
 Vue.directive('highlight', {
   bind(el, binding, vnode) {
-    el.style.backgroundColor = 'green';
+    el.style.backgroundColor = binding.value;
   }
 });
 
