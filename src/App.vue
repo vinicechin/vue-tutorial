669b6332553c6a41e4803a0316dbc9eb255e206d
Vinicius Cechin <vinicechin@gmail.com>
2018-01-30 17:27:15 -0200

    Added Lecture 189
    Setting on-load animation

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@
                 <transition name="slide" type="animation">
                     <div class="alert alert-info" v-show="show">This is some info</div>
                 </transition>
+                <transition name="fade" appear>
+                    <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
             </div>
         </div>
     </div>
@@ -21,7 +24,7 @@
     export default {
         data() {
             return {
-                show: false
+                show: true
             }
         }
     }
