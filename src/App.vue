2dd18732e83254cebe718bad14eec6a01a95f750
Vinicius Cechin <vinicechin@gmail.com>
2018-01-30 17:40:57 -0200

    Added Lecture 190
    Using different css class names

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
                 <transition name="slide" type="animation">
                     <div class="alert alert-info" v-show="show">This is some info</div>
                 </transition>
-                <transition name="fade" appear>
+                <transition appear enter-active-class="animated bounce" leave-active-class="animated shake">
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
             </div>
