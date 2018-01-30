5b8f792f52b97b77646b82146ac29584f24f13e9
Vinicius Cechin <vinicechin@gmail.com>
2018-01-30 11:37:20 -0200

    Added Lecture 166
    Passing complex values

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">Color this</p>
-                <p v-local-highlight:background.delayed.blink="'green'">Color this</p>
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'green', secondColor: 'blue', delay: 500}">Color this</p>
             </div>
         </div>
     </div>
@@ -29,31 +29,29 @@
             }
 
             if (binding.modifiers['blink']) {
-              let mainColor = binding.value;
-              let secondColor = 'blue';
+              let mainColor = binding.value.mainColor;
+              let secondColor = binding.value.secondColor;
               let currentColor = mainColor;
               el.style.color = 'white';
               setTimeout(() => {
                 setInterval(() => {
                   currentColor = (currentColor == secondColor) ? mainColor : secondColor;
                   if (binding.arg == 'background') {
-                    //v-highlight:background
                     el.style.backgroundColor = currentColor;
                   } else {
-                    // no argument passed
                     el.style.color = currentColor;
                   }
-                }, 500);
+                }, binding.value.delay);
               }, delay);
             } else {
               setTimeout(() => {
                 if (binding.arg == 'background') {
                   //v-highlight:background
-                  el.style.backgroundColor = binding.value;
-                  el.style.color = 'white';
+                  el.style.backgroundColor = binding.value.mainColor;
+                  el.style.color = binding.value.secondColor;
                 } else {
                   // no argument passed
-                  el.style.color = binding.value;
+                  el.style.color = binding.value.mainColor;
                 }
               }, delay);
             }
