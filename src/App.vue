ed03c6871cf93baff3c09441ded424e89fb0ca43
Vinicius Cechin <vinicechin@gmail.com>
2018-01-30 13:43:43 -0200

    Added Lecture 170
    Adding filters

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-
+                <p>{{ text | to-uppercase}}</p>
             </div>
         </div>
     </div>
@@ -11,6 +11,17 @@
 
 <script>
     export default {
+      data() {
+        return {
+          text: 'Hello there!'
+        }
+      },
+
+      filters: {
+        'to-uppercase'(value) {
+          return value.toUpperCase();
+        }
+      }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.filter('to-lowercase', (value) => {
+  return value.toLowerCase();
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
