d97a0a5776601064cec1083a3cb2c1cc8582d545
Vinicius Cechin <vinicechin@gmail.com>
2018-01-30 11:17:50 -0200

    Added Lecture 164
    Local custom directive

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">Color this</p>
+                <p v-local-highlight:background.delayed="'green'">Color this</p>
             </div>
         </div>
     </div>
@@ -19,6 +20,27 @@
 
 <script>
     export default {
+      directives: {
+        'local-highlight': {
+          bind(el, binding, vnode) {
+            var delay = 0;
+            if (binding.modifiers['delayed']) {
+              delay = 1000;
+            }
+
+            setTimeout(() => {
+              if (binding.arg == 'background') {
+                //v-highlight:background
+                el.style.backgroundColor = binding.value;
+                el.style.color = 'white';
+              } else {
+                // no argument passed
+                el.style.color = binding.value;
+              }
+            }, delay);
+          }
+        }
+      }
     }
 </script>
 
