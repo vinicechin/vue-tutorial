82f923aa7e6974f1e03b33d3dba33d286f494e61
Vinicius Cechin <vinicechin@gmail.com>
2018-01-30 11:13:14 -0200

    Added Lecture 162
    Using modifiers on custom directive

--- src/main.js
+++ src/main.js
@@ -3,14 +3,21 @@ import App from './App.vue'
 
 Vue.directive('highlight', {
   bind(el, binding, vnode) {
-    if (binding.arg == 'background') {
-      //v-highlight:background
-      el.style.backgroundColor = binding.value;
-      el.style.color = 'white';
-    } else {
-      // no argument passed
-      el.style.color = binding.value;
+    var delay = 0;
+    if (binding.modifiers['delayed']) {
+      delay = 1000;
     }
+
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        //v-highlight:background
+        el.style.backgroundColor = binding.value;
+        el.style.color = 'white';
+      } else {
+        // no argument passed
+        el.style.color = binding.value;
+      }
+    }, delay);
   }
 });
 
