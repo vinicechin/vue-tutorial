5c75281f615cfaae18a770a3d9d1466ff6e8df47
Vinicius Cechin <vinicechin@gmail.com>
2018-01-31 15:26:37 -0200

    Start of new project
    Refactored folders to keep only necessary files

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,21 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Filters & Mixins</h1>
-                <!-- Exercise 1) -->
-                <!-- Build a local Filter which reverses the Text it is applied on -->
-                <p>{{ text | reverseText }}</p>
-                <!-- Exercise 2 -->
-                <!-- Build a global Filter which counts the length of a word and it appends it -->
-                <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
-                <p>{{ text | appendLength }}</p>
-                <!-- Exercise 3 -->
-                <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
-                <p>{{ reversedText }}</p>
-                <p>{{ textWithLength }}</p>
-                <!-- Exercise 4 -->
-                <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1 class="text-center">The Super Quiz</h1>
+      </div>
     </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import { TextMixins } from './TextMixins';
-
-    export default {
-        data() {
-            return {
-              text: 'Hello'
-            }
-        },
-        
-        filters: {
-            reverseText(value) {
-                return value.split("").reverse().join("");
-            }
-        },
-
-        mixins: [TextMixins]
-    }
+  export default {
+  }
 </script>
 
-<style>
-</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.filter('appendLength', (value) => {
-  let cont = value.split("").length;
-  return value + " (" + cont + ")";
-})
-
 new Vue({
   el: '#app',
   render: h => h(App)
