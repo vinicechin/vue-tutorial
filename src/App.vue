cbd8be651bc4d8426f08c158c5dff5b1a5591b14
Vinicius Cechin <vinicechin@gmail.com>
2018-01-31 16:56:55 -0200

    Added changes to project
    Question panel and answer buttons layout

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,25 @@
     <hr>
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        
+        <app-question></app-question>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import AppQuestion from './components/AppQuestion.vue';
+
   export default {
+    data() {
+      return {
+        currentComponent: 'app-question'
+      }
+    },
+
+    components: {
+      'app-question': AppQuestion
+    }
   }
 </script>
 
---
+++ src/components/AppQuestion.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <p class="panel-title text-center">How much is 1 + 1?</p>
+    </div>
+    <div class="panel-body">
+      <div class="col-xs-12 col-sm-4 text-center">
+        <button id="option" class="btn btn-primary btn-lg">2</button>
+      </div>
+      <div class="col-xs-12 col-sm-4 text-center">
+        <button id="option" class="btn btn-primary btn-lg">2</button>
+      </div>
+      <div class="col-xs-12 col-sm-4 text-center">
+        <button id="option" class="btn btn-primary btn-lg">2</button>
+      </div>
+      <div class="col-xs-12 col-sm-4 text-center">
+        <button id="option" class="btn btn-primary btn-lg">2</button>
+      </div>
+      <div class="col-xs-12 col-sm-4 text-center">
+        <button id="option" class="btn btn-primary btn-lg">2</button>
+      </div>
+      <div class="col-xs-12 col-sm-4 text-center">
+        <button id="option" class="btn btn-primary btn-lg">2</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  #option {
+    margin: 5px;
+  }
+</style>
