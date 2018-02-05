c064c1b7146ae6a44da24a2aa53a0e15ef359c2a
Vinicius Cechin <vinicechin@gmail.com>
2018-02-05 16:11:26 -0200

    Added Lecture 226
    Navigating with router links

---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+  <ul class="nav nav-pills">
+    <li class="nav-item"><router-link to="/">Home</router-link></li>
+    <li class="nav-item"><router-link to="/user">User</router-link></li>
+  </ul>
+</template>
