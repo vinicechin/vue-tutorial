cdf866936c7b27784a356c8b74ba788d88938dff
Vinicius Cechin <vinicechin@gmail.com>
2018-02-05 09:57:09 -0200

    Added other possible animation
    Professor animation version

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,12 @@
     <hr>
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <transition enter-active-class="animated bounceInRight" leave-active-class="animated bounceOutRight" mode="out-in">
+        <!-- <transition enter-active-class="animated bounceInRight" leave-active-class="animated bounceOutRight" mode="out-in">
+            <keep-alive>
+                <component :is="currentComponent" :isCorrect="isCorrect" @answered="showAnswer($event)" @showNewQuestion="showQuestion"></component>
+            </keep-alive>
+        </transition> -->
+        <transition name="flip" mode="out-in">
             <keep-alive>
                 <component :is="currentComponent" :isCorrect="isCorrect" @answered="showAnswer($event)" @showNewQuestion="showQuestion"></component>
             </keep-alive>
@@ -51,3 +56,39 @@
     }
   }
 </script>
+
+<style>
+  .flip-enter {
+    /*transform: rotateY(0deg);*/
+  }
+
+  .flip-enter-active {
+    animation: flip-in 0.5s ease-out forwards;
+  }
+
+  .flip-leave {
+    /*transform: rotateY(0deg);*/
+  }
+
+  .flip-leave-active {
+    animation: flip-out 0.5s ease-out forwards;
+  }
+
+  @keyframes flip-out {
+    from {
+      transform: rotateY(0deg);
+    }
+    to {
+      transform: rotateY(90deg);
+    }
+  }
+
+  @keyframes flip-in {
+    from {
+      transform: rotateY(90deg);
+    }
+    to {
+      transform: rotateY(0deg);
+    }
+  }
+</style>
