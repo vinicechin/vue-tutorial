690b85e4d89508e57fc0823d2ba7ff6af78f5891
Vinicius Cechin <vinicechin@gmail.com>
2018-02-05 15:01:19 -0200

    Added Lecture 216
    using default resource

--- src/App.vue
+++ src/App.vue
@@ -31,22 +31,24 @@
             username: '',
             email: ''
           },
-          users: []
+          users: [],
+          resource: {}
         }
       },
 
       methods: {
         submit() {
-          this.$http.post('', this.user)
-            .then(response => {
-              console.log(response);
-            }, error => {
-              console.log(error);
-            });
+          // this.$http.post('data.json', this.user)
+          //   .then(response => {
+          //     console.log(response);
+          //   }, error => {
+          //     console.log(error);
+          //   });
+          this.resource.save({}, this.user);
         },
 
         fetch() {
-          this.$http.get('')
+          this.$http.get('data.json')
             .then(response => {
               return response.json();
             })
@@ -58,6 +60,9 @@
               this.users = array;
             });
         }
+      },
+      created() {
+        this.resource = this.$resource('data.json');
       }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 
 Vue.use(VueResource);
 
-Vue.http.options.root = 'https://vuejs-http-8f333.firebaseio.com/data.json';
+Vue.http.options.root = 'https://vuejs-http-8f333.firebaseio.com/';
 Vue.http.interceptors.push((request, next) => {
   console.log(request);
   if (request.method == 'POST') {
