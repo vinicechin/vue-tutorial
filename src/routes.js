804cda256071c235c08a3122a9dc974ac75c20fb
Vinicius Cechin <vinicechin@gmail.com>
2018-02-06 11:03:10 -0200

    Added Lecture 240
    "Catch-all" routes

--- src/routes.js
+++ src/routes.js
@@ -18,5 +18,6 @@ export const routes = [
     { path: ':id', component: UserDetail },
     { path: ':id/edit', component: UserEdit, name: 'userEdit' }
   ] },
-  { path: 'redirect-me', redirect: { name: 'home' } }
+  { path: 'redirect-me', redirect: { name: 'home' } },
+  { path: '*', redirect: { name: 'home' } }
 ];
