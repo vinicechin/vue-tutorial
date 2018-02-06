d88210f2238aaf0732d9efa3d7778770c57ed804
Vinicius Cechin <vinicechin@gmail.com>
2018-02-06 12:47:20 -0200

    Start of Section 17: Vuex
    Refactore folders structure to only have files related to section 17

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,29 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Filters & Mixins</h1>
-                <!-- Exercise 1) -->
-                <!-- Build a local Filter which reverses the Text it is applied on -->
-                <p>{{ text | reverseText }}</p>
-                <!-- Exercise 2 -->
-                <!-- Build a global Filter which counts the length of a word and it appends it -->
-                <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
-                <p>{{ text | appendLength }}</p>
-                <!-- Exercise 3 -->
-                <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
-                <p>{{ reversedText }}</p>
-                <p>{{ textWithLength }}</p>
-                <!-- Exercise 4 -->
-                <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
+                <h1>Vuex</h1>
+                <app-result :counter="counter"></app-result>
+                <hr>
+                <app-counter @updated="counter += $event"></app-counter>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import { TextMixins } from './TextMixins';
+    import Counter from './components/Counter.vue';
+    import Result from './components/Result.vue';
 
     export default {
         data() {
             return {
-              text: 'Hello'
+                counter: 0
             }
         },
-        
-        filters: {
-            reverseText(value) {
-                return value.split("").reverse().join("");
-            }
-        },
-
-        mixins: [TextMixins]
+        components: {
+            appCounter: Counter,
+            appResult: Result,
+        }
     }
 </script>
 
-<style>
-</style>
