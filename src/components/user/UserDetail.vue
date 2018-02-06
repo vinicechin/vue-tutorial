6cb73ccb820e86ad49deafd59dc8d9aea9bb81cc
Vinicius Cechin <vinicechin@gmail.com>
2018-02-06 10:45:03 -0200

    Added Lecture 237
    Query parameters

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,6 +2,6 @@
     <div>
       <h3>Some User Details</h3>
       <p>User loaded has ID: {{ $route.params.id }}</p>
-      <router-link tag="button" :to="{ name: 'userEdit', params: { id: $route.params.id } }">Edit User</router-link>
+      <router-link tag="button" :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100 } }">Edit User</router-link>
     </div>
 </template>
