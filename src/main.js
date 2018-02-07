7bb718a688860aef03178d012e2d7c3291d4b168
Vinicius Cechin <vinicechin@gmail.com>
2018-02-07 13:26:28 -0200

    Added Lecture 338
    Using Axios interceptors

--- src/main.js
+++ src/main.js
@@ -9,6 +9,21 @@ axios.defaults.baseURL = 'https://vuejs-http-8f333.firebaseio.com';
 axios.defaults.headers.common['Authorization'] = 'adasd';
 axios.defaults.headers.get['Accepts'] = 'application/json'
 
+// add interceptor
+const reqInterID = axios.interceptors.request.use(config => {
+  console.log('Request interceptor', config);
+  return config;
+})
+
+const resInterID = axios.interceptors.response.use(res => {
+  console.log('Response interceptors', res);
+  return res;
+})
+
+// remove interceptor
+axios.interceptors.request.eject(reqInterID);
+axios.interceptors.response.eject(resInterID);
+
 new Vue({
   el: '#app',
   router,
