5cb5712bb2ccc67003e3f13c02354c30be184e30
Vinicius Cechin <vinicechin@gmail.com>
2018-02-08 10:51:55 -0200

    Added Lecture 357
    Auto login and data persistence

--- src/store.js
+++ src/store.js
@@ -44,6 +44,13 @@ export default new Vuex.Store({
             token: res.data.idToken,
             userId: res.data.localId
           });
+
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000);
+          localStorage.setItem('token', res.data.idToken);
+          localStorage.setItem('userId', res.data.localId);
+          localStorage.setItem('expirationDate', expirationDate);
+
           dispatch('storeUser', authData);
           dispatch('setLogoutTimer', res.data.expiresIn);
         })
@@ -69,12 +76,38 @@ export default new Vuex.Store({
             token: res.data.idToken,
             userId: res.data.localId
           })
+
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000);
+          localStorage.setItem('token', res.data.idToken);
+          localStorage.setItem('userId', res.data.localId);
+          localStorage.setItem('expirationDate', expirationDate);
+
           dispatch('setLogoutTimer', res.data.expiresIn);
         })
         .catch(error => console.log(error));
     },
+    tryAutoLogin({commit}) {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        return;
+      }
+      const expirationDate = localStorage.getItem('expirationDate');
+      const now = new Date();
+      if (now >= expirationDate) {
+        return;
+      }
+      const userId = localStorage.getItem('userId');
+      commit('authUser', {
+        token: token,
+        userId: userId
+      })
+    },
     logout({commit}) {
       commit('clearAuthData');
+      localStorage.removeItem('expirationDate');
+      localStorage.removeItem('token');
+      localStorage.removeItem('userId');
       router.replace('/signin');
     },
     fetchUser({commit, state}) {
