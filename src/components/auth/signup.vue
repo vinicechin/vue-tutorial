87a37a4f2423f86726a8ae19810adac6d4d2412a
Vinicius Cechin <vinicechin@gmail.com>
2018-02-08 11:39:41 -0200

    Added Lecture 367
    Numeric and Min validators

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -12,12 +12,14 @@
           <p v-if="!$v.email.email">Please provide a valid email address</p>
           <p v-if="!$v.email.required">This field must not be empty</p>
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.age.$error}">
           <label for="age">Your Age</label>
           <input
                   type="number"
                   id="age"
+                  @blur="$v.age.$touch()"
                   v-model.number="age">
+          <p v-if="!$v.age.minAge">You have to be at least {{ $v.age.$params.minAge.min }} years old</p>
         </div>
         <div class="input">
           <label for="password">Password</label>
@@ -72,7 +74,7 @@
 </template>
 
 <script>
-  import { required, email } from 'vuelidate/lib/validators'
+  import { required, email, numeric, minValue } from 'vuelidate/lib/validators'
   export default {
     data () {
       return {
@@ -89,6 +91,11 @@
       email: {
         required, // required: required,
         email // email: email
+      },
+      age: {
+        required,
+        numeric,
+        minAge: minValue(18)
       }
     },
     methods: {
