f9c28392af85efd397a95d25dc5a97c8b20fa880
Vinicius Cechin <vinicechin@gmail.com>
2018-02-08 09:52:12 -0200

    Added Lecture 354
    Updating UI state based on auth state

--- src/store.js
+++ src/store.js
@@ -83,6 +83,9 @@ export default new Vuex.Store({
   getters: {
     user(state) {
       return state.user;
+    },
+    isAuthenticated(state) {
+      return state.idToken !== null;
     }
   }
 })
