ca667371ebb746e58e836398bb56e49772c33b0d
Vinicius Cechin <vinicechin@gmail.com>
2018-02-08 11:24:15 -0200

    Added Lecture 365
    Validation UI feedback

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -2,13 +2,15 @@
   <div id="signup">
     <div class="signup-form">
       <form @submit.prevent="onSubmit">
-        <div class="input">
+        <div class="input" :class="{invalid: $v.email.$error}">
           <label for="email">Mail</label>
           <input
                   type="email"
                   id="email"
                   @input="$v.email.$touch()"
                   v-model="email">
+          <p v-if="!$v.email.email">Please provide a valid email address</p>
+          <p v-if="!$v.email.required">This field must not be empty</p>
         </div>
         <div class="input">
           <label for="age">Your Age</label>
@@ -156,6 +158,15 @@
     outline: none;
     border: 1px solid #521751;
     background-color: #eee;
+  }
+
+  .input.invalid label {
+    color: red;
+  }
+
+  .input.invalid input {
+    border: 1px solid red;
+    background-color: #ffc9aa;
   }
 
   .input select {
